f7d242851757b045efd81de005fe8c583433e9ad
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-09-18 11:42:43 +0300

    Packaging stuff

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pip.req import parse_requirements
+
+from pyabstractapi.version import get_version()
+
+install_reqs = list(parse_requirements("requirements", session={}))
+
+setup(name="pyabstractapi",
+		version=get_version(),
+		description="A minimalistic framework for accessing restful APIs",
+		author="Šarūnas Navickas",
+		author_email="zaibacu@gmail.com",
+		license="MIT",
+		packages=["pyabstractapi"],
+		install_requires=[str(ir.req) for ir in install_reqs],
+		test_suite="nose.collector",
+		tests_require=["nose"])
