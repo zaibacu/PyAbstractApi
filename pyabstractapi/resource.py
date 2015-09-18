533f99460aa5446102256b78e61adea2a6a084db
Šarūnas Navickas <sarunas.navickas@adform.com>
2015-09-18 11:41:24 +0300

    Don't force-add extension

--- pyabstractapi/resource.py
+++ pyabstractapi/resource.py
@@ -16,7 +16,7 @@ class Resource(object):
 
 	def get_path(self):
 		if self.parent:
-			return self.parent.get_path() + ["{0}.json".format(self.name)]
+			return self.parent.get_path() + [self.name]
 		else:
 			return [self.name]
 
